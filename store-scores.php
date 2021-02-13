894ce300b40e239230f20a227bdeca1589bbc1d0
Steve Fisher <dr.s.m.fisher@gmail.com>
2021-02-13 17:50:10 +0100

    Starting to write some doc

--- store-scores.php
+++ store-scores.php
@@ -1,24 +1,18 @@
 <?php
 
 /**
- * The plugin bootstrap file
+ * The Store Scores plugin bootstrap file
  *
- * This file is read by WordPress to generate the plugin information in the plugin
+ * This is read by WordPress to generate the plugin information in the plugin
  * admin area. This file also includes all of the dependencies used by the plugin,
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              AuthorURI
- * @since             1.0.0
- * @package           Store_Scores
- *
- * @wordpress-plugin
  * Plugin Name:       Store Scores
- * Plugin URI:        PluginURI
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin URI:        https://github.com/fisherab/store-scores 
+ * Description:       For management of club competition
  * Version:           1.0.0
  * Author:            Steve Fisher
- * Author URI:        AuthorURI
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       store-scores
