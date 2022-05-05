d4b7a35712e1f6a27485fae7ba35d8de0b72510a
Steve Fisher <dr.s.m.fisher@gmail.com>
2022-05-06 00:51:21 +0200

    Add Egyptian

---
+++ includes/competition-types/class-store-scores-egyptian-type.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This represents a normal Ladder competition
+ */
+class Store_Scores_Egyptian_Type extends Store_Scores_Competition_Type {
+
+    /**
+     * Need to return all players other than one one specified
+     *
+     * @param integer $comp_id id of the competition custom post
+     * @param integer $player_id id of the player 
+     */
+    public function get_opponents($comp_id, $player_id) {
+        $competitors = get_post_meta($comp_id,'competitors', true);
+        $opponents = array_diff($competitors,[$player_id, 0]);
+        return $opponents;
+    }
+
+    /**
+     * Get unique short name to display to admin
+     */
+    public function get_tag() {
+        return "Egyptian";
+    }
+
+    /** 
+     * Get human desciption of the format
+     */
+    public function get_description() {
+        $html = "<div>";
+        $html .= "<p>Each player is initially assigned a number of ranking points. If a player beats a player of the same ranking then four points are transferred from the loser to the winner. However beating a higher ranked player results in more points being transferred and vice versa. Winning or losing a game may change your handicap but this change has no impact on the ranking points</p>";
+        $html .= "<p>You may challenge any club member to a game, regardless of whether they are on the ladder already or are yet to play their first game. Failure to accept the challenge and play the game within two weeks results in a maximum score being recorded for the challnger and 0 for the person who failed to play. Such results do not go on handicap cards as regular results would. There is one exception: if you challenge someone who is away from the area (such as on holiday) then there is no expectation that they will play.</p>";
+        $html .= "<p>";
+        $html .= "Initial ladder ranking points are set to 100 for those registering before the 1st June, but late registrants start with 96. ";
+        $html .= "A player's Your position is determined by ranking points, then wins, then net wins (#wins – #losses), then who-beat-who amongst the tied players, then net points.";
+        $html .= "</p>";
+        $html .= "</div>";
+        return $html;
+    }
+
+    /**
+     *  Return formatted results
+     *
+     *  $comp_id id of the competition custom post
+     */
+    public function get_results($comp_id) {
+        $bestof = get_post_meta($comp_id, 'bestof', true); 
+        $rankings = [];
+        $results = get_post_meta($comp_id,'result');
+        foreach ($results as $result) { 
+            $you = $result['you'];
+            $you_id = $you['person'];
+            $opp = $result['opp'];
+            $opp_id = $opp['person'];
+            $you_wins = 0;
+            $opp_wins = 0;
+            for ($i = 1; $i <= $bestof; $i++) {
+                if ($you['scores'][$i] > $opp['scores'][$i]) {
+                    $you_wins++;
+                } else {
+                    $opp_wins++;
+                }
+            }
+
+            if (! isset($rankings[$you_id])) {
+                $rankings[$you_id] = [$you_id,100,0,0];
+            }
+            if (! isset($rankings[$opp_id])) {
+                $rankings[$opp_id] = [$opp_id,100,0,0];
+            }
+            $rankings[$you_id][3]++;
+            $rankings[$opp_id][3]++;
+            if ($you_wins > $opp_wins) {
+                $rankings[$you_id][1]++;
+                $rankings[$opp_id][1]--;
+                $rankings[$you_id][2]++;
+            } else {
+                $rankings[$you_id][1]--;
+                $rankings[$opp_id][1]++;
+                $rankings[$opp_id][2]++;
+            } 
+        }
+        usort($rankings, [$this, 'sort_by_points']);
+        $html = "<div><table>";
+        $html .= '<tr><td>Name</td><td>Points</td><td>Wins</td><td>Games</td></tr>';
+        foreach ($rankings as $ranking) {
+            $person_id = $ranking[0];
+            $person = get_user_by("ID", $person_id);
+            $person_name = $person->get('first_name') . ' ' . $person->get('last_name');
+            $html .= '<tr><td>' . $person_name . '</td><td>' . $ranking[1] . '</td><td>' . $ranking[2] . '</td><td>' . $ranking[3] . '</td></tr>';
+        }
+        $html .= '</table></div>';
+        return $html;
+    }
+
+    private function sort_by_points($ar, $br) {
+        $a = $ar[1];
+        $b = $br[1];
+        if ($a === $b) {
+            return 0;
+        }
+        return ($a < $b) ? 1:-1;
+    }
+
+}
